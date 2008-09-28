a18a043813df7a4493581943a23ade7bd02ca84d
Vincent Bernat <bernat@luffy.cx>
2008-09-28 13:04:59 +0200

    Add classifiers and url in setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,18 @@ from distutils.core import setup, Extension
 if __name__ == "__main__":
     setup(name="snimpy",
           version=VERSION,
+          classifiers = [
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: POSIX',
+            'Programming Language :: C',
+            'Programming Language :: Python',
+            'Topic :: System :: Networking',
+            'Topic :: Utilities',
+            ],
+          url='https://trac.luffy.cx/snimpy/',
           description="interactive SNMP tool",
           author="Vincent Bernat",
           author_email="bernat@luffy.cx",
