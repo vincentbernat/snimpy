7a8ab8a93e4162c339cea467e4e0d07005844591
Vincent Bernat <bernat@luffy.cx>
2010-01-22 11:31:35 +0100

    Enhance list-routes example to also use inetCidrRouteTable

--- examples/list-routes.py
+++ examples/list-routes.py
@@ -1,9 +1,23 @@
 #!/usr/bin/snimpy
 
+from socket import inet_ntoa
+
 load("IP-FORWARD-MIB")
 m=M()
 
+print "Using IP-FORWARD-MIB::ipCidrRouteTable..."
 routes = m.ipCidrRouteNextHop
 for x in routes:
     net, netmask, tos, src = x
     print "%15s/%-15s via %-15s src %-15s" % (net, netmask, routes[x], src)
+
+print
+
+print "Using IP-FORWARD-MIB::inetCidrRouteTable..."
+routes = m.inetCidrRouteIfIndex
+for x in routes:
+    dsttype, dst, prefix, oid, nhtype, nh = x
+    if dsttype != "ipv4" or nhtype != "ipv4":
+        print "Non-IPv4 route"
+        continue
+    print "%15s/%-2d via %-15s" % (inet_ntoa(str(dst)), prefix, inet_ntoa(str(nh)))
