cce05015ac2511b14592a276639445230f0c91ae
Vincent Bernat <vincent@bernat.im>
2015-06-18 21:26:30 +0200

    snmp: CommandGenerator should be a singleton
    The initialization of such an object is expensive (so, Snimpy is slow)
    and there seems to trigger a memory leak. Just use a singleton instead.
    
    Related to #33.

--- snimpy/snmp.py
+++ snimpy/snmp.py
@@ -74,6 +74,8 @@ class Session(object):
     session. From such an instance, one can get information from the
     associated agent."""
 
+    _cmdgen = cmdgen.CommandGenerator()
+
     def __init__(self, host,
                  community="public", version=2,
                  secname=None,
@@ -114,7 +116,6 @@ class Session(object):
         """
         self._host = host
         self._version = version
-        self._cmdgen = cmdgen.CommandGenerator()
 
         # Put authentication stuff in self._auth
         if version in [1, 2]:
