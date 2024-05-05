d2492cf4b8011e40c1828e64adf1ec22ee72d9b5
Vincent Bernat <vincent@bernat.ch>
2024-05-05 21:29:38 +0200

    build: rework flake.nix

--- flake.nix
+++ flake.nix
@@ -7,27 +7,28 @@
     inputs.flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = inputs.nixpkgs.legacyPackages."${system}";
+        checkInputs = with pkgs.python3Packages; [ pytest coverage ];
+        buildInputs = with pkgs.python3Packages; [ pkgs.libsmi vcversioner ];
+        propagatedBuildInputs = with pkgs.python3Packages; [ cffi pysnmp-lextudio ipython ];
       in
       {
         packages.default = pkgs.python3Packages.buildPythonPackage {
+          inherit checkInputs buildInputs propagatedBuildInputs;
           name = "snimpy";
           src = self;
           preConfigure = ''
-           # Unfortunately, we cannot build a proper string from what we got in self
-           echo "1.0.0-0-000000000000" > version.txt
+            # Unfortunately, we cannot build a proper string from what we got in self
+            echo "1.0.0-0-000000000000" > version.txt
           '';
-          checkPhase = "pytest";
-          checkInputs = with pkgs.python3Packages; [ pytest coverage ];
-          propagatedBuildInputs = with pkgs.python3Packages; [ cffi pysnmp ipython ];
-          buildInputs = with pkgs; [ libsmi pkgs.python3Packages.vcversioner ];
+          checkPhase = "${pkgs.python3Packages.pytest}/bin/pytest";
         };
         apps.default = { type = "app"; program = "${self.packages."${system}".default}/bin/snimpy"; };
         devShells.default = pkgs.mkShell {
           name = "snimpy-dev";
-          buildInputs = [
-            self.packages."${system}".default.inputDerivation
-            pkgs.python3Packages.ipython
-          ];
+          buildInputs = checkInputs ++ buildInputs ++ propagatedBuildInputs ++
+            [
+              pkgs.python3Packages.ipython
+            ];
         };
       });
 }
