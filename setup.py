0fe5a836d4af2e5d123d18fa1127c906a10260a1
Vincent Bernat <vincent@bernat.ch>
2025-04-05 10:33:16 +0200

    build: depends on pysnmp instead of pysnmp-lextudio
    The Pypi project has been taken over.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         cffi_modules=(not rtd and ["snimpy/smi_build.py:ffi"] or []),
         install_requires=[
             "cffi >= 1.0.0",
-            "pysnmp-lextudio >= 4, < 6",
+            "pysnmp >= 4, < 6",
             "pyasn1 <= 0.6.0",
             'pyasyncore; python_version >= "3.12"',
             "setuptools",
